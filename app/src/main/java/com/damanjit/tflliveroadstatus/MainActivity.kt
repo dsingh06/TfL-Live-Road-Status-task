eceb6ffddaadd240b04b89f298462fb2ebd8789f
dsingh06 <dsingh06@mail.bbk.ac.uk>
2022-09-25 14:34:19 +0100

    Testing my viewBinding setup with the viewmodel. It is not working!
    I made a setup where button press should update the textview as a first step. This is to check that my 'basic' viewModel and observer pattern implementation is working. On button press the textview did not get updated so I placed it outside the observer, just to check and it is not updating there as well! Need to investigate further.
    
    I have left the dataclass alone for now to be implemented later once the update of variables in the viewModel will be working fine.

--- app/src/main/java/com/damanjit/tflliveroadstatus/MainActivity.kt
+++ app/src/main/java/com/damanjit/tflliveroadstatus/MainActivity.kt
@@ -2,6 +2,7 @@ package com.damanjit.tflliveroadstatus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
@@ -9,12 +10,26 @@ import com.damanjit.tflliveroadstatus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // get Reference to viewmodel class so the data survives orientation changes for example.
+    private val mViewModel:MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // This will give access to all the views within tht elayout file.
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
+        binding.resultView.text = "Testing"
+
+        mViewModel.displayName.observe(this
+        ) { change ->
+            binding.resultView.text = change.toString()
+        }
+
+        binding.button.setOnClickListener(View.OnClickListener {
+            mViewModel.getRoadStatus()
+        })
 
     }
 }
