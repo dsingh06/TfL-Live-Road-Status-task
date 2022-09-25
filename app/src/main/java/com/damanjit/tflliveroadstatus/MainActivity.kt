339e48c8d11f5f775750335910d633ada04bba8e
dsingh06 <dsingh06@mail.bbk.ac.uk>
2022-09-25 15:52:40 +0100

    Made changes to when the button is pressed, a busy indicator (progress bar) should show instead. Later will the checks.

--- app/src/main/java/com/damanjit/tflliveroadstatus/MainActivity.kt
+++ app/src/main/java/com/damanjit/tflliveroadstatus/MainActivity.kt
@@ -2,6 +2,7 @@ package com.damanjit.tflliveroadstatus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -12,12 +13,25 @@ class MainActivity : AppCompatActivity() {
 
     // get Reference to viewmodel class so the data survives orientation changes for example.
     private val mViewModel:MainViewModel by viewModels()
+    private val TAG  = "Main Activity";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // This will give access to all the views within the layout file.
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.progressBar.visibility = View.INVISIBLE
+
+        mViewModel.busy.observe(this
+        ) { busyResult ->
+            if (busyResult){
+                binding.button.visibility = View.INVISIBLE
+                binding.progressBar.visibility = View.VISIBLE
+            } else {
+                binding.button.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.INVISIBLE
+            }
+        }
 
         mViewModel.displayName.observe(this
         ) { change ->
