9bf3ab872677faad2c896d0b639e001294a5d470
dsingh06 <dsingh06@mail.bbk.ac.uk>
2022-09-25 13:45:52 +0100

    Added View Binding to gradle and binding initialised in MainActivity to have access to all the views within the layout file.

--- app/src/main/java/com/damanjit/tflliveroadstatus/MainActivity.kt
+++ app/src/main/java/com/damanjit/tflliveroadstatus/MainActivity.kt
@@ -2,10 +2,19 @@ package com.damanjit.tflliveroadstatus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.damanjit.tflliveroadstatus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+
     }
 }
